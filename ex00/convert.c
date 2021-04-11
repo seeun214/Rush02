b21dc5b98b7564a1bb5a9e54f1fff467a45c4736
Kwanhong Lee <kwalee@c8r7s9.42seoul.kr>
2021-04-11 19:10:12 +0900

    1

--- ex00/convert.c
+++ ex00/convert.c
@@ -6,89 +6,33 @@
 /*   By: keokim <keokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 18:27:30 by keokim            #+#    #+#             */
-/*   Updated: 2021/04/11 11:43:58 by keokim           ###   ########.fr       */
+/*   Updated: 2021/04/11 19:06:30 by kwalee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush.h"
-#include <stdio.h>
 
-void	ft_write_str(char *num, char *buffer, int last)
-{
-	int		i;
-	int		size;
-	char	*start_str;
+int	g_unit;
 
-	start_str = ft_strstr(buffer, num);
-	size = ft_strlen(num);
-	i = 0;
-	while (start_str[i] && start_str[i] != '\n')
-	{
-		while (start_str[i] != ':')
-			i++;
-		if (start_str[i] == ':')
-			i++;
-		while (start_str[i] == ' ')
-			i++;
-		while (start_str[i] >= 32 && start_str[i] <= 126)
-		{
-			write(1, &start_str[i], 1);
-			i++;
-		}
-	}
-	if (last != 0)
-		write(1, " ", 1);
-}
-
-void	ft_write_char(char c, char *buffer, int end)
-{
-	int		i;
-	int		size;
-	char	*start_str;
-	char	num[2];
-
-	num[0] = c;
-	start_str = ft_strstr(buffer, num);
-	size = ft_strlen(num);
-	i = 0;
-	while (start_str[i] && start_str[i] != '\n')
-	{
-		while (start_str[i] != ':')
-			i++;
-		if (start_str[i] == ':')
-			i++;
-		while (start_str[i] == ' ')
-			i++;
-		while (start_str[i] >= 32 && start_str[i] <= 126)
-		{
-			write(1, &start_str[i], 1);
-			i++;
-		}
-	}
-	if (end == 0)
-		write(1, " ", 1);
-}
-
-int		print_all_zero(char *buffer, char *str, int i, int len, int unit)
+void	print_all_zero(char *buffer, char *str, int i, int len)
 {
 	if (str[i] != '0')
 	{
 		ft_write_char(str[i], buffer, 0);
 		print_unit(buffer, len - i);
-		unit = 1;
+		g_unit = 1;
 	}
 	else
 	{
-		if (unit == 0)
+		if (g_unit == 0)
 		{
 			print_unit(buffer, len - i);
-			unit = 1;
+			g_unit = 1;
 		}
 	}
-	return (unit);
 }
 
-int		print_all_one(char *buffer, char *str, int i, int len, int *unit)
+int		print_all_one(char *buffer, char *str, int i, int len)
 {
 	char temp[3];
 
@@ -96,28 +40,28 @@ int		print_all_one(char *buffer, char *str, int i, int len, int *unit)
 	temp[2] = 0;
 	if (str[i] == '1')
 	{
-	   temp[1] = str[i + 1];
-	   ft_write_str(temp, buffer, len - i - 2);
-	   i++;
-	   if (len - i - 1 != 0)
-	   {
-		   print_unit(buffer, len - i);
-		   *unit = 1;
-	   }
+		temp[1] = str[i + 1];
+		ft_write_str(temp, buffer, len - i - 2);
+		i++;
+		if (len - i - 1 != 0)
+		{
+			print_unit(buffer, len - i);
+			g_unit = 1;
+		}
 	}
 	else if (str[i] != '0')
 	{
 		temp[1] = '0';
-		ft_write_str(temp, buffer, 1);
+		ft_write_str(temp, buffer, len - i - 2);
 	}
 	return (i);
 }
 
 void	print_unit(char *buffer, int zamt)
 {
-    char unit[40];
-	int j;
- 
+	char	unit[40];
+	int		j;
+
 	unit[0] = '1';
 	j = 1;
 	while (j < zamt)
@@ -129,7 +73,7 @@ void	print_unit(char *buffer, int zamt)
 	ft_write_str(unit, buffer, 1);
 }
 
-int    print_all_sub(char *buffer, char *str, int i, int len, int unit)
+int		print_all_sub(char *buffer, char *str, int i, int len)
 {
 	if (len - i - 1 == 0)
 	{
@@ -137,34 +81,33 @@ int    print_all_sub(char *buffer, char *str, int i, int len, int unit)
 			ft_write_char(str[i], buffer, 1);
 	}
 	else if ((len - i - 1) % 3 == 2)
-	{	
+	{
 		if (str[i] != '0')
-		{	
+		{
 			ft_write_char(str[i], buffer, 0);
-			ft_write_str("100", buffer,1 );
+			ft_write_str("100", buffer, 1);
 		}
 	}
 	else if ((len - i - 1) % 3 == 1)
 	{
 		if (str[i] != '0')
-			i = print_all_one(buffer, str, i, len, &unit);
+			i = print_all_one(buffer, str, i, len);
 	}
 	else
-		unit = print_all_zero(buffer, str, i ,len, unit);
+		print_all_zero(buffer, str, i, len);
 	return (i);
 }
 
-void    print_all(char *buffer, char *str)
+void	print_all(char *buffer, char *str)
 {
-    int i;
-    int len;
-	int unit;
+	int i;
+	int len;
 
-    len = ft_strlen(str);
-	unit = 0;
-    i = -1;
-    while (++i < len)
-    {
-		i =	print_all_sub(buffer, str, i, len, unit);
-    }
+	len = ft_strlen(str);
+	g_unit = 0;
+	i = -1;
+	while (++i < len)
+	{
+		i = print_all_sub(buffer, str, i, len);
+	}
 }
