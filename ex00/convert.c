7a28e6a69a7730fd7b9d889d5be5d51024c7d012
Kwanhong Lee <kwalee@c8r7s9.42seoul.kr>
2021-04-11 21:44:00 +0900

    1

--- ex00/convert.c
+++ ex00/convert.c
@@ -6,7 +6,7 @@
 /*   By: keokim <keokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 18:27:30 by keokim            #+#    #+#             */
-/*   Updated: 2021/04/11 21:01:27 by kwalee           ###   ########.fr       */
+/*   Updated: 2021/04/11 21:43:03 by kwalee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,8 @@ int		print_all_sub(char *buffer, char *str, int i, int len)
 	{
 		if (str[i] != '0')
 		{
+			if (str[i + 1] != '0' || str[i + 2] != '0')
+				len++;
 			ft_write_char(str[i], buffer, 0);
 			ft_write_str("100", buffer, len - i - 3);
 		}
