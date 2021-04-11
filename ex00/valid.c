753325137a65ae74a2bf6d8854ac51baec7dcdcf
Keonwoo Kim <keokim@c7r1s10.42seoul.kr>
2021-04-11 10:58:45 +0900

    valid

--- ex00/valid.c
+++ ex00/valid.c
@@ -6,7 +6,7 @@
 /*   By: keokim <keokim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 11:42:54 by keokim            #+#    #+#             */
-/*   Updated: 2021/04/10 14:54:05 by keokim           ###   ########.fr       */
+/*   Updated: 2021/04/11 10:57:07 by keokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,12 @@ int		is_num(char c)
 	return ('0' <= c && c <= '9');
 }
 
+#include <stdio.h>
 int		is_valid_num(char *num)
 {
+	char	*nbr;
+
+	nbr = num;
 	if (!num || (*num == '0' && *(num + 1)))
 		return (0);
 	while (*num)
@@ -32,5 +36,7 @@ int		is_valid_num(char *num)
 			return (0);
 		num++;
 	}
+	if (ft_strlen(nbr) > 39)
+		return (0);
 	return (1);
 }
